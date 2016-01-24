398d433a1143078b30d723e630d7072b900c2d75
Dani Bohm <dandi@hotmail.hu>
2016-01-24 18:09:46 +0100

    improve readability

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,20 +10,20 @@ sudo ln -s /usr/bin/vim.basic /etc/alternatives/editor
 # Enable "page up" and "page down" to search the history
 sudo sed -i 's/# \(.*\)history-search\(.*\)/\1history-search\2/g' /etc/inputrc
 
+echo "------------------------------------------------------------------------------"
 echo "Apply nginx settings"
 mkdir /etc/nginx/bkp
 mv /etc/nginx/sites-enabled/default /etc/nginx/bkp
-cat << EOF > /etc/nginx/sites-enabled/pince
-server {
-    location        / {
-        proxy_pass      http://localhost:8001;
-    }
-}
-EOF
+mv /etc/nginx/sites-available/default /etc/nginx/bkp
+#cat << EOF > /etc/nginx/sites-enabled/pince
+#server {
+#    location        / {
+#        proxy_pass      http://localhost:8001;
+#    }
+#}
+#EOF
 
-echo "Setup PHP"
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php5/fpm/php.ini
-
+echo "------------------------------------------------------------------------------"
 echo "Copy EngineX config files"
 cp /vagrant/nginx_conf/enabled/default /etc/nginx/sites-enabled/
 cp /vagrant/nginx_conf/available/default /etc/nginx/sites-available/
@@ -32,6 +32,7 @@ echo "--------------------------------------------------------------------------
 echo "Setup PHP"
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php5/fpm/php.ini
 
+echo "------------------------------------------------------------------------------"
 echo "Restart services"
 service php5-fpm restart
 service nginx restart
